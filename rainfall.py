b09371d0d80f9630b8916b5664d0d036241b56b9
gumina-csuglobal <joshua.gumina@csuglobal.edu>
2024-01-05 20:14:39 -0700

    Basic requirements done

--- rainfall.py
+++ rainfall.py
@@ -7,3 +7,32 @@ The inner loop will iterate twelve times, once for each month.
 Each iteration of the inner loop will ask the user for the inches of rainfall for that month. 
 After all iterations, the program should display the number of months, the total inches of rainfall, and the average rainfall per month for the entire period.
 """
+
+MONTHS = 12
+
+
+def main():
+    years = int(input("Please input number of years of rainfall to enter:\n"))
+
+    rainfall_history=[]
+
+    for year in range(years):     
+        
+        rainfall_history.append([])
+        for month in range(MONTHS):
+            inches = float(input(f"Enter inches of rainfall for year {year}, month {month}: "))
+            rainfall_history[year].append(inches) 
+
+    total_months = years*MONTHS
+    total_inches = sum(map(sum,rainfall_history))
+    average_rainfall = total_inches/total_months
+
+    print(f"Over {total_months} months there were {total_inches} inches of rain, with an average of {average_rainfall} inches of rain per month.")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
