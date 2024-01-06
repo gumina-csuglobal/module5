53ab97f553f8c0a354022ece8d3ef136c2f2b41d
gumina-csuglobal <joshua.gumina@csuglobal.edu>
2024-01-05 20:00:07 -0700

    Init commit

---
+++ bookstore.py
@@ -0,0 +1,11 @@
+"""
+The CSU Global Bookstore has a book club that awards points to its students based on the number of books purchased each month. The points are awarded as follows:
+
+If a customer purchases 0 books, they earn 0 points.
+If a customer purchases 2 books, they earn 5 points.
+If a customer purchases 4 books, they earn 15 points.
+If a customer purchases 6 books, they earn 30 points.
+If a customer purchases 8 or more books, they earn 60 points.
+Write a program that asks the user to enter the number of books that they have purchased this month and then display the number of points awarded.
+
+"""
